cf358b0f5ca87708634c18902410b014832914bf
devsjc <47188100+devsjc@users.noreply.github.com>
2022-11-25 08:18:41 +0000

    Create generator for workflows

---
+++ gen-workflow.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#define parameters which are passed in.
+NAME=$1
+URL=$2
+CRON=$3
+
+#define the template.
+cat  << EOF
+name: Generate, build and publish $NAME list to Pages
+
+on:
+  schedule:
+    - cron: '$CRON'
+  workflow_dispatch:
+
+jobs:
+  generate-and-publish:
+    uses: devsjc/awesome-starsorted/.github/workflows/_template.yml@main
+    with:
+      list-name: $NAME
+      list-url: $URL
+    secrets: inherit
+
+EOF
